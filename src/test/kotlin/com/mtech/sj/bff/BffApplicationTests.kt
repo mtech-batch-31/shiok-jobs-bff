b6eebeac0c3a211364c6bf253f9a50dbadde960a
Wang Shiji <scglwsj@163.com>
2023-09-09 00:12:50 +0800

    gradle init

---
+++ src/test/kotlin/com/mtech/sj/bff/BffApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mtech.sj.bff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
